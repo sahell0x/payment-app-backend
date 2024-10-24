2a46864fa88dd9682254edee090fba7f7b95d97f
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-24 20:44:17 +0530

    added jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const User = require("./db");
 const userRout = require("./routes/userRout");
+const acountRout = require("./routes/acountRout");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -8,8 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/v1",userRout);
+app.use("/api/v1/user",userRout);
+app.use("/api/v1/acount",acountRout);
 
+app.use((err,req,res,next)=>{
+   res.status(500).send("internal server error");
+});
 
 app.listen(3000,()=>{
     console.log("app is listenig");
--- routes/acountRout.js
+++ routes/acountRout.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/signup",(req,res)=>{
-    res.send("hello there");
-})
-
 
 
 module.exports = router;
