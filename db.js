923f35daf3a86cd11eba2910f2534604b2200f74
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-24 13:39:07 +0530

    added constraints

--- db.js
+++ db.js
@@ -1,38 +1,43 @@
 const mongoose = require("mongoose");
 
-(async function(){
-   try{
-    await mongoose.connect("mongodb+srv://leeugaming34:9826405638@cluster0.ftlll.mongodb.net/payment");
-   }
-   catch{
-        console.log("failed to connect with db")
-   }
+(async function () {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://leeugaming34:9826405638@cluster0.ftlll.mongodb.net/payment"
+    );
+  } catch {
+    console.log("failed to connect with db");
+  }
 })();
 
-mongoose.connection.on('connected', () => {
-    console.log('MongoDB connected');
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
 });
 
-mongoose.connection.on('disconnected', () => {
-    console.error('MongoDB disconnected! Trying to reconnect...');
+mongoose.connection.on("disconnected", () => {
+  console.error("MongoDB disconnected! Trying to reconnect...");
 });
 
-mongoose.connection.on('error', (err) => {
-    console.error('MongoDB connection error:', err);
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
 });
 
-mongoose.connection.on('reconnected', () => {
-    console.log('MongoDB reconnected');
+mongoose.connection.on("reconnected", () => {
+  console.log("MongoDB reconnected");
 });
-
 
 const userSchema = mongoose.Schema({
-    userName:String,
-    passWord:String,
-    firstName:String,
-    lastName:String
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  passWord: { type: String, minLength: 3, maxLength: 30,required:true },
+  firstName: {type:String,trim:true,required:true},
+  lastName: {type:String,trim:true,required:true},
 });
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports= {User};
+module.exports = { User };
