0a574636c1716812b2976e2a4524de7e6d22c272
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-24 20:30:04 +0530

    added cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const User = require("./db");
 const userRout = require("./routes/userRout");
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1",userRout);
 
