f112ff9a538e9e81144ff8e278cf581c2a635547
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-25 21:44:53 +0530

    now signup rout can return jwt

--- routes/userRout.js
+++ routes/userRout.js
@@ -1,13 +1,23 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+const User = require("../db");
 const express = require("express");
 const userTypes = require("../types");
 const signUpMiddleware = require("../middlewares/signupMiddleWare");
 
 const router = express.Router();
 
-router.post("/signup",signUpMiddleware,(req,res)=>{
-    res.send("user created!!");
-})
+router.post("/signup", signUpMiddleware, async (req, res) => {
+  try {
+        const response = await User.create(req.body);
 
+           const userToken = jwt.sign({userName:response.userName},secret);
+           res.status(200).json({jwt_token:userToken});
 
+  } catch (e){
+    console.log(e);
+    res.status(400).send("somthing want wrong from catch");
+  }
+});
 
 module.exports = router;
