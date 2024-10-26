e4666ba434c98629735afdcb863dd52da6bcebfb
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-26 19:38:34 +0530

    jwt decode token

--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -1,2 +1,21 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
+
+const authMiddleWare = (req,res,next)=>{
+    const authHeader = req.headers.authorizations;
+    try{
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(403).json({});
+        }
+        
+        const token = authHeader.split(" ")[1];
+        const decode = jwt.verify(token,secret);
+
+    }catch(e){
+        console.log(e);
+        return res.status.json({
+            massage:"server error"
+        })
+
+    }
+}
