989e414858843679193c7262e2f0344d3d915f73
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-27 20:22:29 +0530

    added sign in route

---
+++ route fucntions/signInRoute.js
@@ -0,0 +1,25 @@
+module.exports = async (req,res)=>{
+    const body = req.body;
+     try{
+      
+      const response = await User.findOne({email:body.email});
+   
+      if(response){
+        const isMatched = await bcrypt.compare(body.password,response.password);
+        if(isMatched){
+          return res.status(200).json({
+            massage:"login successfully"
+          })
+        }
+      }
+   
+      return res.status(400).json({
+       massage:"invalid email or password",
+      })
+     }catch(e){
+        console.log(e);
+        res.status(500).json({
+          massage:"internal server error"
+        })
+     }
+  }
--- routes/userRout.js
+++ routes/userRout.js
@@ -6,35 +6,12 @@ const express = require("express");
 const signUpMiddleware = require("../middlewares/signupMiddleWare");
 const signinMiddleWare = require("../middlewares/signinMiddleWare");
 const singUpRoute = require("../route fucntions/singUpRoute");
+const signInRoute = require("../route fucntions/signInRoute");
 
 const router = express.Router();
 
 router.post("/signup", signUpMiddleware, singUpRoute);
 
-router.post("/signin",signinMiddleWare,async (req,res)=>{
-  const body = req.body;
-   try{
-    
-    const response = await User.findOne({email:body.email});
- 
-    if(response){
-      const isMatched = await bcrypt.compare(body.password,response.password);
-      if(isMatched){
-        return res.status(200).json({
-          massage:"login successfully"
-        })
-      }
-    }
- 
-    return res.status(400).json({
-     massage:"invalid email or password",
-    })
-   }catch(e){
-      console.log(e);
-      res.status(500).json({
-        massage:"internal server error"
-      })
-   }
-})
+router.post("/signin",signinMiddleWare,signInRoute);
 
 module.exports = router;
