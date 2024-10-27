df6c8e74bf8eef51957697a7be2662252dafbde0
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-27 21:51:46 +0530

    searchroute done

--- route fucntions/searchRoute.js
+++ route fucntions/searchRoute.js
@@ -1,3 +1,23 @@
-module.exports = async (req,res)=>{
+const User = require("../db");
 
+module.exports = async (req,res)=>{
+    const filter = req.query.filter || "";
+    try{
+        const users = await User.find({
+            $or:[
+                {
+                    firstName:{$regex:filter,$options:"i"}
+
+                },{
+                    lastName:{$regex:filter,$options:"i"}
+                }
+            ]
+        });
+
+        res.status(200).json({users:users});
+
+    }catch(e){
+        console.log(e);
+        res.status(403).json({users:[]});
+    }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -7,11 +7,14 @@ const signUpMiddleware = require("../middlewares/signupMiddleWare");
 const signinMiddleWare = require("../middlewares/signinMiddleWare");
 const singUpRoute = require("../route fucntions/singUpRoute");
 const signInRoute = require("../route fucntions/signInRoute");
+const searchRoute = require("../route fucntions/searchRoute");
 
 const router = express.Router();
 
 router.post("/signup", signUpMiddleware, singUpRoute);
 
 router.post("/signin",signinMiddleWare,signInRoute);
+
+router.get("/bulk",searchRoute);
 
 module.exports = router;
