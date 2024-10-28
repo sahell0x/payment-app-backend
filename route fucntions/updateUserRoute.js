323877c98bacd8b741ec6dd2aed3dd907a1f6ed0
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-28 20:02:39 +0530

    user update route is done

--- route fucntions/updateUserRoute.js
+++ route fucntions/updateUserRoute.js
@@ -1,11 +1,21 @@
-const { json } = require("express");
-
+const User = require("../db");
+const {updateUserTypes} = require("../types");
 module.exports = async (req,res)=>{
     try{
+        const body = req.body;
+        const {success} = updateUserTypes.safeParse(body);
+        if(success){
+            const id = req.userId;
+            await User.findOneAndUpdate({_id:id},body);
+            
+            return res.status(200).json({
+                massage:"user information updated successfully"
+            })
+        }
 
-        const id = req.userId;
-
-
+        return res.status(403).json({
+            massage:"invalid inputs"
+        })
      
 
     }catch(e){
