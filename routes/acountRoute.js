eded6c631a7168acdff46852de67fc21d68b69ca
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-29 22:47:14 +0530

    added transfer route in acount route

--- routes/acountRoute.js
+++ routes/acountRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const authMiddleWare = require("../middlewares/authMiddleWare");
 const transferMiddleWare = require("../middlewares/transferMiddleWare");
 const balanceRoute = require("../route fucntions/balanceRoute");
+const transferRoute = require("../route fucntions/transferRoute");
 const router = express.Router();
 
 router.get("/balance",authMiddleWare,balanceRoute);
 
-router.post("/transfer",authMiddleWare,transferMiddleWare)
+router.post("/transfer",authMiddleWare,transferMiddleWare,);
 
 module.exports = router;
