22e736b105c10a50a51840acdb25c27ac571fa18
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-29 21:35:20 +0530

    added some more zod validation for decimal places

--- middlewares/transferMIddleWare.js
+++ middlewares/transferMIddleWare.js
@@ -1,1 +1,5 @@
 const transferTypes = require("../types");
+
+module.exports = (req,res)=>{
+    const body = req.body;
+}
--- types.js
+++ types.js
@@ -1,26 +1,36 @@
 const z = require("zod");
 
 const signUpTypes = z.object({
-    email: z.string().max(50).email(),
-    password: z.string().min(6),
-    firstName: z.string().min(3).max(30),
-    lastName: z.string().min(3).max(30),
-  });
+  email: z.string().max(50).email(),
+  password: z.string().min(6),
+  firstName: z.string().min(3).max(30),
+  lastName: z.string().min(3).max(30),
+});
 
-  const signInTypes = z.object({
-    email:z.string().email(),
-    password:z.string()
-  })
+const signInTypes = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
 
-  const updateUserTypes = z.object({
-    password: z.string().min(6).optional(),
-    firstName: z.string().min(3).max(30).optional(),
-    lastName: z.string().min(3).max(30).optional()
-  })
+const updateUserTypes = z.object({
+  password: z.string().min(6).optional(),
+  firstName: z.string().min(3).max(30).optional(),
+  lastName: z.string().min(3).max(30).optional(),
+});
 
-  const transferTypes = z.object({
-    amount:z.number().min(1),
-    to:z.string()
-  });
+const transferTypes = z.object({
+  amount: z.number().refine(
+    (n) => {
+      const decimalPart = n.toString().split(".")[1];
+      return !decimalPart || decimalPart.length <= 2;
+    },
+    {
+      message:
+        "Only integers or floats with up to 2 decimal places are allowed",
+    }
+  ),
 
-  module.exports= {signUpTypes,signInTypes,updateUserTypes,transferTypes};
+  to: z.string(),
+});
+
+module.exports = { signUpTypes, signInTypes, updateUserTypes, transferTypes };
