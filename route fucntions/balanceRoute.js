b3ac9cd6ee28656f3febcaef56d6c93064c07863
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-29 15:30:52 +0530

    balace route done

--- route fucntions/balanceRoute.js
+++ route fucntions/balanceRoute.js
@@ -1,6 +1,16 @@
 const {Acount} = require("../db");
-module.exports = (req,res)=>{
+module.exports = async(req,res)=>{
     try{
+        const userId = req.userId;
+        const response = await Acount.findOne({userId:userId});
+
+        if(response){
+            return res.status(200).json({
+                balance:response.balance
+            })
+        }
+
+        return res.status(403).json({});
 
     }catch(e){
         console.log(e);
--- routes/acountRoute.js
+++ routes/acountRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const authMiddleWare = require("../middlewares/authMiddleWare");
+const balanceRoute = require("../route fucntions/balanceRoute");
 const router = express.Router();
 
-router.get("/balance",authMiddleWare)
-
-
+router.get("/balance",authMiddleWare,balanceRoute);
 
 module.exports = router;
