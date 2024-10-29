b96fff906e97bc7e6ccec160ad1edf183ba126d2
sahell0x <sahil.sk.khan6872@gmail.com>
2024-10-29 21:49:10 +0530

    transfer middleware done

--- middlewares/transferMIddleWare.js
+++ middlewares/transferMIddleWare.js
@@ -1,8 +1,20 @@
 const transferTypes = require("../types");
 
-module.exports = (req,res)=>{
-    const body = req.body;
-    const {success} = transferTypes.safeParse({
-        
-    });
+module.exports = (req,res,next)=>{
+    try{
+        const body = req.body;
+         const {success} = transferTypes.safeParse(body);
+         if(success){
+            next();
+         }
+         return res.status(400).json({
+            massage:"invalid inputs",
+         });
+
+    }catch(e){
+        console.log(e);
+        return res.status(400).json({
+            massage:"bad request"
+        });
+    }
 }
